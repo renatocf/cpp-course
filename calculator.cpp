f1ccf77bd0c6f92d40dbef1b075f2d7a33451c07
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2016-03-20 22:30:44 -0300

    Reading values from the input
    As a rule, any interesting application accepts data from its user, process it
    and shows its results in some useful way. In C++, it is possible to read values
    from the console using the command `cin` (from Console INput), defined in the
    `std` namespace and available by the `iostream` header. `>>` is the **input
    operator**, used by `cin` to point to the variables that will store the values
    read. The type of the variable being read determine the format that has to be
    written by the user in the console. In the case of signed integers, the format
    written has to be the same of integer literals.
    
    The expected output is: Two messages prompting the user for numbers to be used
    in the sum, subtraction, product and division, then the result of executing
    these operations. In the following examples, we will keep using `20` and `10`
    as our standard input.
    
    Command lines:
    ================================================================================
    $ CXX -std=c++14 calculator.cpp -o calculator
    $ ./calculator
    Input 1st number (a): 20
    Input 2nd number (b): 10
    The result of `a + b` is: 30
    The result of `a - b` is: 10
    The result of `a * b` is: 200
    The result of `a / b` is: 2
    ================================================================================

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 int main() {
-  int a = 20, b = 10;
+  int a, b;
+
+  std::cout << "Input 1st number (a): ";
+  std::cin >> a;
+  std::cout << "Input 2nd number (b): ";
+  std::cin >> b;
+
   std::cout << "The result of `a + b` is: " << a + b << std::endl;
   std::cout << "The result of `a - b` is: " << a - b << std::endl;
   std::cout << "The result of `a * b` is: " << a * b << std::endl;
